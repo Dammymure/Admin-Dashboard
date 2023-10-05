89864027d1067ab92d4acc754423e2eae157d0dd
Dammymure <dammymure1@gmail.com>
2023-10-05 09:09:05 +0100

    Made use of Syncfusion Ui to build the interface and charts

---
+++ src/pages/Employees.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  GridComponent,
+  ColumnDirective,
+  Page,
+  Search,
+  Inject,
+  Toolbar
+} from "@syncfusion/ej2-react-grids";
+
+import { employeesData, employeesGrid } from "../data/dummy";
+
+import { Header } from "../components";
+import { ColumnsDirective } from "@syncfusion/ej2-react-kanban";
+
+const Employees = () => {
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={['Search']}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[Toolbar, Page, Search]}
+        />
+      </GridComponent>
+    </div>
+  );
+};
+
+export default Employees;
